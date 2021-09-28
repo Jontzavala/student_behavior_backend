1cf3af364c44cbd4ee8feac055efdb2392811667
Jonathan Zavala <jontzavala@gmail.com>
2021-09-28 17:48:53 -0500

    figuring out behaviors

--- app/controllers/behaviors_controller.rb
+++ app/controllers/behaviors_controller.rb
@@ -1,2 +1,6 @@
 class BehaviorsController < ApplicationController
+    #def index
+        #behaviors = Behavior.all
+        #render json: behaviors
+    #end
 end
--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,4 +1,5 @@
 class StudentSerializer < ActiveModel::Serializer
   attributes :id, :name, :seat_number, :course_id
+  #has_many :behaviors
   belongs_to :course
 end
