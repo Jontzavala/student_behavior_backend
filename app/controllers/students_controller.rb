8e403ec68887e4873127a54110cfcca03d9557f6
Jonathan Zavala <jontzavala@gmail.com>
2021-09-28 23:43:04 -0500

    working on create for students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,14 @@ class StudentsController < ApplicationController
         students = Student.all
         render json: students
     end
+
+    def create
+        student = Student.new(student_params)
+    end
+
+
+    private
+
+    def student_params
+    end
 end
